bdd908d32da562a4b1aca43a95e66bfb14955f88
chenlou <1061788688@qq.com>
2022-05-31 11:54:34 +0800

    UDF 自定义函数 TableFunction

--- src/main/scala/org/excitinglab/flink/tableAndSQL/udf/TableFunctionTest.scala
+++ src/main/scala/org/excitinglab/flink/tableAndSQL/udf/TableFunctionTest.scala
@@ -1,5 +1,8 @@
 package org.excitinglab.flink.tableAndSQL.udf
 
+import com.alibaba.fastjson.{JSON, JSONArray, JSONObject}
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.api.scala.typeutils.Types
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
 import org.apache.flink.table.functions.TableFunction
@@ -39,6 +42,31 @@ object TableFunctionTest {
       .print()
 
     env.execute()
+
+  }
+
+  class MyFlatMapFunction extends TableFunction[Row] {
+
+    override def getResultType: TypeInformation[Row] = {
+      Types.ROW(Types.STRING, Types.STRING, Types.STRING, Types.STRING)
+    }
+
+    def eval(val1: String, val2: String, val3: String): Unit = {
+      val json = JSON.parse(val3).asInstanceOf[JSONArray]
+      json.forEach(x => {
+        val temp = x.asInstanceOf[JSONObject]
+        val keyIter = temp.keySet().iterator()
+        val row: Row = Row.withPositions(4)
+        row.setField(0, val1)
+        row.setField(1, val2)
+        while (keyIter.hasNext) {
+          val key = keyIter.next()
+          row.setField(2, key)
+          row.setField(3, temp.get(key))
+        }
+        collect(row)
+      })
+    }
 
   }
 
