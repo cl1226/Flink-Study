ac9b5645f22d05c054f9df6f7b55363264c6e632
chenlou <1061788688@qq.com>
2022-06-07 09:49:23 +0800

    table api elasticsearch7 sink

---
+++ src/main/scala/org/excitinglab/flink/connectors/table/Elasticsearch.scala
@@ -0,0 +1,48 @@
+package org.excitinglab.flink.connectors.table
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
+import org.apache.flink.types.Row
+import org.apache.flink.streaming.api.scala._
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object Elasticsearch {
+
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val tableEnv = StreamTableEnvironment.create(env)
+
+    tableEnv.executeSql(
+      s"""
+         |CREATE TABLE myUserTable (
+         |  `@timestamp` STRING,
+         |  message STRING
+         |) WITH (
+         |  'connector' = 'elasticsearch-7',
+         |  'hosts' = 'http://node03:9200',
+         |  'index' = 'flink-es-2022.06.07'
+         |)
+         |""".stripMargin)
+
+    val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+    val stream = env.socketTextStream("node05", 8888)
+      .flatMap(_.split(" "))
+      .map(x => {
+        (format.format(new Date()), x)
+      })
+
+    val table = tableEnv.fromDataStream(stream)
+
+    tableEnv.registerTable("test", table)
+
+    tableEnv.executeSql("insert into myUserTable select * from test")
+
+    env.execute()
+  }
+
+}
